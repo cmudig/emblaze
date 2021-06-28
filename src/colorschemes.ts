ae4f2ac3c363956c9b567edd137031bed9d85c81
Venkatesh Sivaraman <venkatesh.sivaraman.98@gmail.com>
2021-06-27 23:34:20 -0400

    Lint colorschemes

--- src/colorschemes.ts
+++ src/colorschemes.ts
@@ -1,12 +1,12 @@
 // import * as d3 from "d3";
-const d3 = require("d3");
+const d3 = require('d3');
 
 class ColorScheme {
   name: string;
   value: Function;
   type: string;
 
-  constructor(name: string, value: Function, type = "continuous") {
+  constructor(name: string, value: Function, type = 'continuous') {
     this.name = name;
     this.value = value;
     this.type = type;
@@ -14,16 +14,16 @@ class ColorScheme {
 }
 
 let colorSchemes: Array<ColorScheme> = [
-  new ColorScheme("turbo", d3.interpolateTurbo),
-  new ColorScheme("tableau", d3.schemeTableau10, "categorical"),
-  new ColorScheme("plasma", d3.interpolatePlasma),
-  new ColorScheme("magma", d3.interpolateMagma),
-  new ColorScheme("viridis", d3.interpolateViridis),
-  new ColorScheme("RdBu", d3.interpolateRdBu),
-  new ColorScheme("Blues", d3.interpolateBlues),
-  new ColorScheme("Greens", d3.interpolateGreens),
-  new ColorScheme("Reds", d3.interpolateReds),
-  new ColorScheme("rainbow", d3.interpolateRainbow),
+  new ColorScheme('turbo', d3.interpolateTurbo),
+  new ColorScheme('tableau', d3.schemeTableau10, 'categorical'),
+  new ColorScheme('plasma', d3.interpolatePlasma),
+  new ColorScheme('magma', d3.interpolateMagma),
+  new ColorScheme('viridis', d3.interpolateViridis),
+  new ColorScheme('RdBu', d3.interpolateRdBu),
+  new ColorScheme('Blues', d3.interpolateBlues),
+  new ColorScheme('Greens', d3.interpolateGreens),
+  new ColorScheme('Reds', d3.interpolateReds),
+  new ColorScheme('rainbow', d3.interpolateRainbow),
 ];
 
 function getColorScheme(name: string): ColorScheme | undefined {
@@ -33,8 +33,8 @@ function getColorScheme(name: string): ColorScheme | undefined {
 export default {
   getColorScheme,
   defaultColorScheme(dataType: string): ColorScheme | undefined {
-    if (dataType == "categorical") return getColorScheme("tableau");
-    return getColorScheme("plasma");
+    if (dataType == 'categorical') return getColorScheme('tableau');
+    return getColorScheme('plasma');
   },
   allColorSchemes: colorSchemes.map((v) => v),
 };
