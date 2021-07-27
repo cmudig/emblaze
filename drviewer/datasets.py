e87a8f52d704176b123c176f10893f320e2b608c
Venkatesh Sivaraman <venkatesh.sivaraman.98@gmail.com>
2021-07-27 12:01:19 -0400

    Use default metric for EmbeddingSet.compute_neighbors

--- drviewer/datasets.py
+++ drviewer/datasets.py
@@ -359,7 +359,7 @@ class EmbeddingSet:
 
         return EmbeddingSet(lo_ds, align=align and not pre_aligned)
     
-    def compute_neighbors(self, n_neighbors=10, metric='euclidean'):
+    def compute_neighbors(self, n_neighbors=10, metric=None):
         """
         Computes and saves a set of nearest neighbors in each embedding set according
         to the Field.POSITION values. This can be accessed after completing this
